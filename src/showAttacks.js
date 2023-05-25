9a595e914c964b3596afcbf6fb7ebc201102e07a
Liaksej <tarusov.liaksej@gmail.com>
2023-05-25 13:58:22 +0200

    Initial commit

---
+++ src/showAttacks.js
@@ -0,0 +1,11 @@
+export default function showAttacks({special}) {
+    const attacksList = [];
+    special.forEach(element => {
+        if (!("description" in element)) {
+            element.description = undefined;
+        }
+        const {id, name, description, icon} = element
+        attacksList.push({id, name, description, icon})
+    })
+    return attacksList
+}
